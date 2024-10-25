f119a81b90ac5d31635740c1f0e1e845b6c40f3f
elalamielhassaniotmane <elalamielhassani@gmail.com>
2024-10-25 21:38:57 +0200

    first commit

---
+++ src/test/java/com/otmane/Tondeuse/TondeuseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.otmane.Tondeuse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TondeuseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
